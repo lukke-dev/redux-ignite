523f886b5c1fce7ed7c5d04c222039c0152d14a8
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-08-12 10:30:35 -0300

    Trocando Redux por zustand

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,22 +1,24 @@
 import ReactPlayer from 'react-player'
-import { useAppDispatch, useAppSelector } from "../store";
 import { Loader } from "lucide-react";
-import { next, useCurrentLesson } from "../store/slices/player";
+import { useCurrentLesson, useStore } from '../zustand-store';
 
 export function Video() {
-  const dispatch = useAppDispatch()
-
   const { currentLesson } = useCurrentLesson()
 
-  const isCourseLoading = useAppSelector((state) => state.player.isLoading)
+  const { isLoading, next } = useStore((state) => {
+    return {
+      isLoading: state.isLoading,
+      next: state.next
+    }
+  })
 
   function handlePlayNext() {
-    dispatch(next())
+    next()
   }
 
   return (
     <div className="w-full bg-zinc-950 aspect-video">
-      {isCourseLoading ? (
+      {isLoading ? (
         <div className="flex h-full items-center justify-center">
           <Loader className="w-6 h-6 text-zinc-400 animate-spin" />
         </div>
