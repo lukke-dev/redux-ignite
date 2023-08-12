03ea305e405f0ea7095880e1b8c80080ab91d198
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-08-12 09:18:08 -0300

    push

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+import { store } from "./store";
+import { Player } from "./pages/Player";
+import { Provider as ReduxProvider } from "react-redux";
+
+
+export function App() {
+  return (
+    <ReduxProvider store={store}>
+      <Player />
+    </ReduxProvider>
+  )
+}
+
